073d0c6c03f80d4d0092eaaf487dc29e6e6493e4
VitalyBorin <vitaly.borin@gmail.com>
2020-12-29 01:15:57 +0300

    lesson_1

--- dz2.py
+++ dz2.py
@@ -3,4 +3,6 @@ time_h=time_sec_in // 3600
 time_m=time_sec_in % 3600 // 60
 time_s=time_sec_in % 3600 % 60
 
-print('Время в формате Ч:М:С - ', time_h, ':', time_m, ':', time_s, sep='')
+#print('Время в формате Ч:М:С - ', time_h, ':', time_m, ':', time_s, sep='')
+#print(f"{time_h}:{time_m}:{time_s}")
+print('Время в формате ЧЧ:ММ:СС - ', f"{time_h}:{'{:02}'.format(time_m)}:{'{:02}'.format(time_s)}")
